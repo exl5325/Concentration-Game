3755dbaa00941ac828e91c5fbc6ae59771881e92
exl5325 <exl5325@cs.rit.edu>
2016-11-14 01:11:11 -0500

    Finished - comments need to be added

--- gui/ConcentrationGUI.java
+++ gui/ConcentrationGUI.java
@@ -23,17 +23,18 @@ import model.ConcentrationModel;
 /**
  * Created by Eric LIn on 11/3/2016.
  */
-public class ConcentrationGUI extends Application implements Observer{
-    private ConcentrationModel model = new ConcentrationModel();
+public class ConcentrationGUI extends Application implements Observer {
+    private ConcentrationModel model;
     private ArrayList<Button> buttons = new ArrayList<>();
-    private ArrayList<Card> cards = model.getCards();
+    private ArrayList<Button> cheatButtons = new ArrayList<>();
+    private ArrayList<Card> cards;
+    private ArrayList<Card> cardsClone = new ArrayList<>();
     private ArrayList<String> images = new ArrayList<>();
-    Label alabel = new Label( "Pick a card!" );
-    private final Label counter = new Label("Total Moves: 0");
+    private Label alabel = new Label("Select the first card.");
+    private final Label counter = new Label("0 Moves");
+    private int cardsFaceUp = 0;
 
-
-
-    public ConcentrationGUI(){
+    public ConcentrationGUI() {
     }
 
     /**
@@ -53,70 +54,67 @@ public class ConcentrationGUI extends Application implements Observer{
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         BorderPane b = new BorderPane();
         b.setCenter(makeButtonPane());
-        alabel.setFont( new Font( "Helvetica", 14 ) );
-        alabel.setAlignment( Pos.CENTER_RIGHT );
+        alabel.setFont(new Font("Helvetica", 14));
+        alabel.setAlignment(Pos.CENTER);
         b.setTop(alabel);
         b.setBottom(this.makeBottomButtons());
-        images.add("img2.png");
-        images.add("img3.png");
-        images.add("img4.png");
-        images.add("img5.png");
-        images.add("img6.png");
-        images.add("img7.png");
-        images.add("img8.png");
-        images.add("img9.png");
+        images.add("resources/img2.png");
+        images.add("resources/img3.png");
+        images.add("resources/img4.png");
+        images.add("resources/img5.png");
+        images.add("resources/img6.png");
+        images.add("resources/img7.png");
+        images.add("resources/img8.png");
+        images.add("resources/img9.png");
         Scene scene = new Scene(b);
         primaryStage.setTitle("Concentration");
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 
     @Override
-    public void init() throws Exception{
+    public void init() throws Exception {
+        model = new ConcentrationModel();
         this.model.reset();
         model.addObserver(this);
+        cards = model.getCards();
+
     }
 
-    private Pane makeButtonPane(){
+    private Pane makeButtonPane() {
         GridPane grid = new GridPane();
-        for(int rows = 0; rows < 4; rows++ ){
-            for(int col = 0; col < 4; col++){
+        for (int rows = 0; rows < 4; rows++) {
+            for (int col = 0; col < 4; col++) {
                 Button btn = new Button();
-                Image img1 = new Image(getClass().getResourceAsStream("img1.png"));
+                Image img1 = new Image(getClass().getResourceAsStream("resources/img1.png"));
                 btn.setGraphic(new ImageView(img1));
                 btn.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-                int n = rows + col * 4;
-                System.out.println("N:" + n);
-                btn.setOnAction((event) -> {model.selectCard(n);});
+                int n = col + rows * 4;
+                btn.setOnAction((event) -> {
+                    model.selectCard(n);
+                });
                 buttons.add(btn);
-                grid.add(btn, rows, col);
+                grid.add(btn, col, rows);
             }
         }
         return grid;
     }
 
-
-    /**
-     * This method is called whenever the observed object is changed. An
-     * application calls an <tt>Observable</tt> object's
-     * <code>notifyObservers</code> method to have all the object's
-     * observers notified of the change.
-     *
-     * @param o   the observable object. (not used)
-     * @param arg an argument passed to the <code>notifyObservers</code> (not used)
-     */
-    @Override
-    public void update(Observable o, Object arg) {
-        counter.setText(String.format("Total Moves: " + model.getMoveCount()));
-        for(Button b:buttons){
-            int i = buttons.indexOf(b);
-            int j = cards.get(i).getNumber();
-            if(j != -1){
-                alabel.setText("Pick the second card!");
-                switch (j){
+    private void graphics(ArrayList<Button> listOfButtons, Boolean isCheat){
+        for (Button b : listOfButtons) {
+            int i = listOfButtons.indexOf(b);
+            int j;
+            if(!isCheat){
+                j = cards.get(i).getNumber();
+            }
+            else {
+                j = cardsClone.get(i).getNumber();
+            }
+            if (j != -1) {
+                switch (j) {
                     case 0:
                         Image img1 = new Image(getClass().getResourceAsStream(images.get(0)));
                         b.setGraphic(new ImageView(img1));
@@ -150,27 +148,101 @@ public class ConcentrationGUI extends Application implements Observer{
                         b.setGraphic(new ImageView(img8));
                         break;
                 }
-            }
-            else{
-                Image img1 = new Image(getClass().getResourceAsStream("img1.png"));
+            } else {
+                Image img1 = new Image(getClass().getResourceAsStream("resources/img1.png"));
                 b.setGraphic(new ImageView(img1));
             }
         }
     }
-    private Pane makeBottomButtons(){
+
+    private int howManyFaceUp(){
+        cardsFaceUp = 0;
+        for(Card c:cards){
+            if(c.isFaceUp()){
+                cardsFaceUp++;
+            }
+        }
+        return cardsFaceUp;
+    }
+
+
+    /**
+     * This method is called whenever the observed object is changed. An
+     * application calls an <tt>Observable</tt> object's
+     * <code>notifyObservers</code> method to have all the object's
+     * observers notified of the change.
+     *
+     * @param o   the observable object. (not used)
+     * @param arg an argument passed to the <code>notifyObservers</code> (not used)
+     */
+    @Override
+    public void update(Observable o, Object arg) {
+        counter.setText(String.format(model.getMoveCount() + " Moves"));
+
+        if(model.howManyCardsUp() == 0){
+            alabel.setText("Select the first card.");
+            if(howManyFaceUp() == 16){
+                alabel.setText("YOU WIN!");
+            }
+        }else if(model.howManyCardsUp() == 1){
+            alabel.setText("Select the second card.");
+        }else if(model.howManyCardsUp() == 2){
+            alabel.setText("No match: Undo or select a card.");
+        }
+        graphics(buttons, false);
+    }
+
+
+    private Pane makeBottomButtons() {
         HBox grid = new HBox();
         Button undo = new Button("Undo");
         undo.setOnAction((javafx.event.ActionEvent event) -> model.undo());
         Button cheat = new Button("Cheat");
-        cheat.setOnAction((javafx.event.ActionEvent event) -> model.cheat());
+        cheat.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                Stage stage = new Stage();
+                model.cheat();
+                for(Card c:cards){
+                    cardsClone.add(new Card(c));
+                }
+                GridPane cheater = new GridPane();
+                for(Card c: cardsClone){
+                    c.setFaceUp();
+                }
+                for (int rows = 0; rows < 4; rows++) {
+                    for (int col = 0; col < 4; col++) {
+                        Button btn = new Button();
+                        cheatButtons.add(btn);
+                        cheater.add(btn, col, rows);
+                    }
+                }
+                graphics(cheatButtons, true);
+                cardsClone.clear();
+                cheatButtons.clear();
+                Scene cheats = new Scene(cheater);
+                stage.setScene(cheats);
+                stage.setTitle("Cheat Window");
+                stage.show();
+            }
+        });
         Button reset = new Button("Reset");
-        reset.setOnAction((javafx.event.ActionEvent event) -> model.reset());
+        reset.setOnAction((javafx.event.ActionEvent event) -> {
+            model.reset();
+            cards = model.getCards();
+            alabel.setText("Select the first card.");
+            cardsFaceUp = 0;
+        });
         Region space = new Region();
+        Region anotherSpace = new Region();
         HBox.setHgrow(space, Priority.ALWAYS);
-        grid.getChildren().addAll(undo, cheat, reset, space, counter);
+        HBox.setHgrow(anotherSpace, Priority.ALWAYS);
+        grid.getChildren().addAll(anotherSpace, undo, cheat, reset, space, counter);
         grid.setAlignment(Pos.CENTER);
         return grid;
     }
+
+
 
 
     public static void main(String[] args) {
